1e63cd66e572556945e8c04b0bea197abd731117
Timothy Click <tclick@nctu.edu.tw>
2019-01-11 13:55:42 +0800

    Refactored FluctSCA class.
    Signed-off-by: Timothy Click <tcthepoet@yahoo.com>

--- src/fluctmatch/fluctsca/fluctsca.py
+++ src/fluctmatch/fluctsca/fluctsca.py
@@ -15,64 +15,31 @@
 # doi:10.1016/bs.mie.2016.05.024.
 #
 import numpy as np
-from scipy import linalg
+from numpy.random import RandomState
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import scale, StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.utils.validation import check_array, check_is_fitted, FLOAT_DTYPES
 
 from ..decomposition.ica import ICA
 from ..decomposition.svd import SVD
+from .finddims import FindDims
 
 
 class FluctSCA(BaseEstimator, TransformerMixin):
     def __init__(self, n_components: int=None, max_iter: int=1000,
-                 whiten: bool=True, stddev: float = 2.0,
-                 method: str="extended-infomax"):
+                 whiten: bool=True, stddev: int= 2., tol: float=0.99,
+                 method: str="infomax", svd_solver: str="auto",
+                 random_state: RandomState=None):
         super().__init__()
         self.n_components: int= n_components
         self.max_iter: int= max_iter
-        self.stddev: float= stddev
+        self.stddev: int= stddev
         self.whiten: bool= whiten
+        self.tol: float= tol
         self.method: str= method
-
-    def _randomize(self, X: np.ndarray) -> np.ndarray:
-        """Calculates eigenvalues from a random matrix.
-
-        Parameters
-        ----------
-
-        """
-        X: np.ndarray = check_array(X, copy=True, dtype=FLOAT_DTYPES)
-        _, n_windows = X.shape
-
-        mean: np.ndarray = np.mean(X, axis=-1)[:, None]
-        mean: np.ndarray = np.tile(mean, (1, n_windows)),
-        std: np.ndarray = np.std(X, axis=-1)[:, None]
-        std: np.ndarray = np.tile(std, (1, n_windows)),
-        positive: bool = np.all(X >= 0.)
-
-        svd: SVD = SVD()
-        Lrand: np.ndarray = np.empty((self.max_iter, np.min(X.shape)), dtype=X.dtype)
-        for _ in range(self.max_iter):
-            Y: np.ndarray = np.random.normal(mean, std)
-            if positive:
-                Y[Y < 0.] = 0.
-            if self.whiten:
-                Y: np.ndarray = scale(Y)
-            Lrand[_, :] = svd.fit(Y).explained_variance_
-        return Lrand
-
-    def _calculate_maxdims(self, X: np.ndarray):
-        """Calculate the significant number of eigenvalues.
-        """
-        X: np.ndarray = check_array(X, accept_sparse=('csr', 'csc'), copy=True,
-                                    warn_on_dtype=True, estimator=self,
-                                    dtype=FLOAT_DTYPES,
-                                    force_all_finite='allow-nan')
-        value: float = X[:, 1].mean() + ((self.stddev + 1) * X[:, 1].std())
-        self.n_components: int = self.eigenvector_[self.eigenvector_ > value].size
+        self.svd_solver: str= svd_solver
+        self.random_state: RandomState= random_state
 
     def fit(self, X: np.ndarray) -> "FluctSCA":
         from sklearn.pipeline import Pipeline
@@ -82,39 +49,46 @@ class FluctSCA(BaseEstimator, TransformerMixin):
                                     estimator=self, dtype=FLOAT_DTYPES,
                                     force_all_finite='allow-nan')
 
-        svd: SVD = SVD()
+        if self.n_components is None:
+            fd: FindDims = FindDims(whiten=self.whiten, max_iter=self.max_iter,
+                                    stddev=self.stddev, tol=self.tol,
+                                    algorithm=self.svd_solver,
+                                    random_state=self.random_state)
+            self.n_components_ = fd.fit_transform(X)
+        else:
+            self.n_components_ = self.n_components
+
+        # Perform singular value decomposition
+        svd: SVD = SVD(n_components=self.n_components_)
         pipeline: Pipeline = (
             make_pipeline(StandardScaler(), svd)
             if self.whiten
             else make_pipeline(svd)
         )
         pipeline.fit(X)
-        self.eigenvector_ = svd.explained_variance_
-
-        if self.n_components < 1 or self.n_components is None:
-            Lrand: np.ndarray= self._randomize(X)
-            self._calculate_maxdims(Lrand)
+        self.Vfeatures_: np.ndarray = svd.components_.T
+        self.singular_values_: np.ndarray = svd.singular_values_.copy()
 
         return self
 
     def transform(self, X: np.ndarray, copy: bool=True) -> np.ndarray:
         from sklearn.pipeline import Pipeline
 
-        check_is_fitted(self, "Lsca")
+        check_is_fitted(self, "Vfeatures_")
         X: np.ndarray = check_array(X, accept_sparse=('csr', 'csc'), copy=copy,
                         warn_on_dtype=True, estimator=self, dtype=FLOAT_DTYPES,
                         force_all_finite='allow-nan')
 
         ica: ICA = ICA(n_components=self.n_components, method=self.method,
                        whiten=self.whiten)
-        self.sources_: np.ndarray = ica.fit_transform(X)
 
-        # Perform truncated singular value decomposition
-        svd: SVD = SVD(n_components=self.n_components)
-        pipeline: Pipeline = (
-            make_pipeline(StandardScaler(), svd)
-            if self.whiten
-            else make_pipeline(svd)
-        )
-        self.U_: np.ndarray = pipeline.fit_transform(X)
-        self.components_: np.ndarray = svd.components_
+        Usamples_: np.ndarray= X.dot(self.Vfeatures_ / self.singular_values_)
+
+        # Perform ICA on SVD feature covariance matrix
+        Vfica: np.ndarray = ica.fit_transform(self.Vfeatures_)
+        # Project feature matrix onto sample covariance matrix.
+        self.Ufica_: np.ndarray = np.dot(ica.mixing_, Usamples_.T).T
+        # Perform ICA on sample covariance matrix
+        self.Usica_: np.ndarray = ica.fit_transform(Usamples_)
+
+        return Vfica
