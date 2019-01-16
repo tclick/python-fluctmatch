3a751b876f958158c283e4c0f265fa6c068ca870
Timothy Click <tclick@nctu.edu.tw>
2019-01-16 15:53:50 +0800

    Updated copyright Refactored code to match ModelBase Refactored test code to match solvent module
    Signed-off-by: Timothy Click <tcthepoet@yahoo.com>

--- src/fluctmatch/models/solvent.py
+++ src/fluctmatch/models/solvent.py
@@ -1,133 +1,196 @@
 # -*- coding: utf-8 -*-
-# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
-#
-# fluctmatch --- https://github.com/tclick/python-fluctmatch
-# Copyright (c) 2013-2017 The fluctmatch Development Team and contributors
-# (see the file AUTHORS for the full list of names)
-#
-# Released under the New BSD license.
-#
-# Please cite your use of fluctmatch in published work:
-#
-# Timothy H. Click, Nixon Raj, and Jhih-Wei Chu.
-# Calculation of Enzyme Fluctuograms from All-Atom Molecular Dynamics
-# Simulation. Meth Enzymology. 578 (2016), 327-342,
-# doi:10.1016/bs.mie.2016.05.024.
-#
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals,
-)
-from future.builtins import (
-    super,
-    zip,
-)
 
-from collections import OrderedDict
+#  python-fluctmatch -
+#  Copyright (c) 2019 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 
-from MDAnalysis.core import topologyattrs
-from fluctmatch.models.base import ModelBase
+#
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#  Please cite your use of fluctmatch in published work:
+#
+#  Timothy H. Click, Nixon Raj, and Jhih-Wei Chu.
+#  Calculation of Enzyme Fluctuograms from All-Atom Molecular Dynamics
+#  Simulation. Meth Enzymology. 578 (2016), 327-342,
+#  doi:10.1016/bs.mie.2016.05.024.
+
+from typing import List, Mapping, Tuple
+
+import numpy as np
+from MDAnalysis.core.topologyattrs import Atomtypes, Bonds
+
+from .base import ModelBase
 
 
 class Water(ModelBase):
-    """Create a universe consisting of the water oxygen.
-    """
-    model = "WATER"
-    describe = "c.o.m./c.o.g. of whole water molecule"
-    _mapping = OrderedDict()
+    """Create a universe consisting of the water oxygen."""
+    model: str = "WATER"
+    describe: str = "c.o.m./c.o.g. of whole water molecule"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._mapping["OW"] = "water"
+    def __init__(self,
+                 xplor: bool = True,
+                 extended: bool = True,
+                 com: bool = True,
+                 guess_angles: bool = True,
+                 cutoff: float = 10.0):
+        super().__init__(xplor, extended, com, guess_angles, cutoff)
+        self._mapping["OW"]: str = "water"
 
-        kwargs["guess_bonds"] = False
-        kwargs["mapping"] = self._mapping
-        self._initialize(*args, **kwargs)
-        self.atoms.select_atoms("name OW").types = 1
+        self.guess: bool = False
+
+    def _add_atomtypes(self):
+        n_atoms: int = self.universe.atoms.n_atoms
+        atomtypes: Atomtypes = Atomtypes(np.ones(n_atoms))
+        self.universe.add_TopologyAttr(atomtypes)
 
     def _add_bonds(self):
-        self._topology.add_TopologyAttr(topologyattrs.Bonds([]))
-        self._generate_from_topology()
+        self.universe._topology.add_TopologyAttr(Bonds([]))
+        self.universe._generate_from_topology()
 
 
 class Tip3p(ModelBase):
     """Create a universe containing all three water atoms."""
-    model = "TIP3P"
-    describe = "All-atom watter"
-    _mapping = OrderedDict()
+    model: str = "TIP3P"
+    describe: str = "All-atom watter"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._mapping["OW"] = "name OW MW"
-        self._mapping["HW1"] = "name HW1"
-        self._mapping["HW2"] = "name HW2"
+    def __init__(self,
+                 xplor: bool = True,
+                 extended: bool = True,
+                 com: bool = True,
+                 guess_angles: bool = True,
+                 cutoff: float = 10.0):
+        super().__init__(xplor, extended, com, guess_angles, cutoff)
+        self._mapping["OW"]: str = "name OW MW"
+        self._mapping["HW1"]: str = "name HW1"
+        self._mapping["HW2"]: str = "name HW2"
+        self._types: Mapping[str, int] = dict(OW=1, HW1=2, HW2=3)
 
-        kwargs["mapping"] = self._mapping
-        self._initialize(*args, **kwargs)
-        self.atoms.select_atoms("name OW").types = 1
-        self.atoms.select_atoms("name HW1").types = 2
-        self.atoms.select_atoms("name HW2").types = 3
+    def _add_atomtypes(self):
+        atomtypes: List[int] = [
+            self._types[atom.name] for atom in self.universe.atoms
+        ]
+        self.universe.add_TopologyAttr(Atomtypes(atomtypes))
 
     def _add_bonds(self):
-        bonds = []
+        bonds: List[Tuple[int, int]] = []
         bonds.extend([
-            _ for s in self.segments for _ in zip(
-                s.atoms.select_atoms("name OW").ix,
-                s.atoms.select_atoms("name HW1").ix)
+            _
+            for s in self.universe.segments
+            for _ in zip(s.atoms.select_atoms("name OW").ix,
+                         s.atoms.select_atoms("name HW1").ix)
         ])
         bonds.extend([
-            _ for s in self.segments for _ in zip(
-                s.atoms.select_atoms("name OW").ix,
-                s.atoms.select_atoms("name HW2").ix)
+            _
+            for s in self.universe.segments
+            for _ in zip(s.atoms.select_atoms("name OW").ix,
+                         s.atoms.select_atoms("name HW2").ix)
         ])
         bonds.extend([
-            _ for s in self.segments for _ in zip(
-                s.atoms.select_atoms("name HW1").ix,
-                s.atoms.select_atoms("name HW2").ix)
+            _
+            for s in self.universe.segments
+            for _ in zip(s.atoms.select_atoms("name HW1").ix,
+                         s.atoms.select_atoms("name HW2").ix)
         ])
-        self._topology.add_TopologyAttr(topologyattrs.Bonds(bonds))
-        self._generate_from_topology()
+        self.universe._topology.add_TopologyAttr(Bonds(bonds))
+        self.universe._generate_from_topology()
 
 
 class Dma(ModelBase):
-    """Create a universe for N-dimethylacetamide.
-    """
-    model = "DMA"
-    describe = "c.o.m./c.o.g. of C1, N, C2, and C3 of DMA"
-    _mapping = OrderedDict()
+    """Create a universe for N-dimethylacetamide."""
+    model: str = "DMA"
+    describe: str = "c.o.m./c.o.g. of C1, N, C2, and C3 of DMA"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._mapping["C1"] = "resname DMA and name C1 H1*"
-        self._mapping["N"] = "resname DMA and name C N O"
-        self._mapping["C2"] = "resname DMA and name C2 H2*"
-        self._mapping["C3"] = "resname DMA and name C3 H3*"
+    def __init__(self,
+                 xplor: bool = True,
+                 extended: bool = True,
+                 com: bool = True,
+                 guess_angles: bool = True,
+                 cutoff: float = 10.0):
+        super().__init__(xplor, extended, com, guess_angles, cutoff)
+        self._mapping["C1"]: str = "resname DMA and name C1 H1*"
+        self._mapping["N"]: str = "resname DMA and name C N O"
+        self._mapping["C2"]: str = "resname DMA and name C2 H2*"
+        self._mapping["C3"]: str = "resname DMA and name C3 H3*"
+        self._types: Mapping[str, int] = dict(C1=4, N=5, C2=6, C3=7)
 
-        kwargs["mapping"] = self._mapping
-        self._initialize(*args, **kwargs)
-        self.atoms.select_atoms("name C1").types = 4
-        self.atoms.select_atoms("name N").types = 5
-        self.atoms.select_atoms("name C2").types = 6
-        self.atoms.select_atoms("name C3").types = 7
+    def _add_atomtypes(self):
+        atomtypes: List[int] = [
+            self._types[atom.name] for atom in self.universe.atoms
+        ]
+        self.universe.add_TopologyAttr(Atomtypes(atomtypes))
 
     def _add_bonds(self):
-        bonds = []
+        bonds: List[Tuple[int, int]] = []
         bonds.extend([
-            _ for s in self.segments for _ in zip(
-                s.atoms.select_atoms("name C1").ix,
-                s.atoms.select_atoms("name N").ix)
+            _
+            for s in self.universe.segments
+            for _ in zip(s.atoms.select_atoms("name C1").ix,
+                         s.atoms.select_atoms("name N").ix)
         ])
         bonds.extend([
-            _ for s in self.segments for _ in zip(
-                s.atoms.select_atoms("name C2").ix,
-                s.atoms.select_atoms("name N").ix)
+            _
+            for s in self.universe.segments
+            for _ in zip(s.atoms.select_atoms("name C2").ix,
+                         s.atoms.select_atoms("name N").ix)
         ])
         bonds.extend([
-            _ for s in self.segments for _ in zip(
-                s.atoms.select_atoms("name C3").ix,
-                s.atoms.select_atoms("name N").ix)
+            _
+            for s in self.universe.segments
+            for _ in zip(s.atoms.select_atoms("name C3").ix,
+                         s.atoms.select_atoms("name N").ix)
         ])
-        self._topology.add_TopologyAttr(topologyattrs.Bonds(bonds))
-        self._generate_from_topology()
+        self.universe._topology.add_TopologyAttr(Bonds(bonds))
+        self.universe._generate_from_topology()
--- tests/models/test_solvent.py
+++ tests/models/test_solvent.py
@@ -1,185 +1,195 @@
 # -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
-# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
+#  python-fluctmatch -
+#  Copyright (c) 2019 Timothy H. Click, Ph.D.
 #
-# fluctmatch --- https://github.com/tclick/python-fluctmatch
-# Copyright (c) 2013-2017 The fluctmatch Development Team and contributors
-# (see the file AUTHORS for the full list of names)
+#  All rights reserved.
 #
-# Released under the New BSD license.
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
 #
-# Please cite your use of fluctmatch in published work:
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
 #
-# Timothy H. Click, Nixon Raj, and Jhih-Wei Chu.
-# Calculation of Enzyme Fluctuograms from All-Atom Molecular Dynamics
-# Simulation. Meth Enzymology. 578 (2016), 327-342,
-# doi:10.1016/bs.mie.2016.05.024.
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
 #
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals,
-)
-from future.utils import native_str
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+#
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#  Please cite your use of fluctmatch in published work:
+#
+#  Timothy H. Click, Nixon Raj, and Jhih-Wei Chu.
+#  Calculation of Enzyme Fluctuograms from All-Atom Molecular Dynamics
+#  Simulation. Meth Enzymology. 578 (2016), 327-342,
+#  doi:10.1016/bs.mie.2016.05.024.
+
+from typing import List
 
 import MDAnalysis as mda
 from numpy import testing
-from fluctmatch.models import (
-    ions,
-    solvent,
-)
+from fluctmatch.models import solvent
 from fluctmatch.models.selection import *
-from tests.datafiles import (
-    TIP3P,
-    TIP4P,
-    IONS,
-    DMA,
-)
+from ..datafiles import TIP3P, TIP4P, DMA
 
 
 def test_water_from_tip3p_creation():
-    aa_universe = mda.Universe(TIP3P)
-    cg_universe = solvent.Water(TIP3P)
-    cg_natoms = (aa_universe.select_atoms("name OW").n_atoms)
-    testing.assert_equal(
-        cg_universe.atoms.n_atoms,
-        cg_natoms,
-        err_msg=native_str("Number of sites don't match."),
-        verbose=True,
-    )
+    aa_universe: mda.Universe = mda.Universe(TIP3P)
+    water: solvent.Water = solvent.Water()
+    water.create_topology(aa_universe)
+
+    n_atoms: int = sum(aa_universe.select_atoms(selection).residues.n_residues
+                       for selection in water._mapping.values())
+
+    testing.assert_equal(water.universe.atoms.n_atoms, n_atoms,
+        err_msg="Number of sites don't match.", verbose=True)
 
 
 def test_water_from_tip3p_positions():
-    positions = []
-    aa_universe = mda.Universe(TIP3P)
-    cg_universe = solvent.Water(TIP3P)
-    for _ in aa_universe.select_atoms("name OW HW* MW").residues:
-        positions.append(
-            _.atoms.select_atoms("name OW HW* MW").center_of_mass())
+    aa_universe: mda.Universe = mda.Universe(TIP3P)
+    water: solvent.Water = solvent.Water()
+    cg_universe: mda.Universe = water.transform(aa_universe)
+
+    positions: List[np.ndarray] = [
+        _.atoms.select_atoms(selection).center_of_mass()
+        for _ in aa_universe.select_atoms("water").residues
+        for selection in water._mapping.values()
+    ]
+
     testing.assert_allclose(
-        np.array(positions),
+        np.asarray(positions),
         cg_universe.atoms.positions,
-        err_msg=native_str("The coordinates do not match."),
+        err_msg="The coordinates do not match.",
     )
 
 
 def test_water_from_tip4p_creation():
-    aa_universe = mda.Universe(TIP4P)
-    cg_universe = solvent.Water(TIP4P)
-    cg_natoms = (aa_universe.select_atoms("name OW").n_atoms)
-    testing.assert_equal(
-        cg_universe.atoms.n_atoms,
-        cg_natoms,
-        err_msg=native_str("Number of sites don't match."),
-        verbose=True,
-    )
+    aa_universe: mda.Universe = mda.Universe(TIP4P)
+    water: solvent.Water = solvent.Water()
+    water.create_topology(aa_universe)
+
+    n_atoms: int = sum(aa_universe.select_atoms(selection).residues.n_residues
+                       for selection in water._mapping.values())
+
+    testing.assert_equal(water.universe.atoms.n_atoms, n_atoms,
+        err_msg="Number of sites don't match.", verbose=True)
 
 
 def test_water_from_tip4p_positions():
-    positions = []
-    aa_universe = mda.Universe(TIP4P)
-    cg_universe = solvent.Water(TIP4P)
-    for _ in aa_universe.select_atoms("name OW HW* MW").residues:
-        positions.append(
-            _.atoms.select_atoms("name OW HW* MW").center_of_mass())
+    aa_universe: mda.Universe = mda.Universe(TIP4P)
+    water: solvent.Water = solvent.Water()
+    cg_universe: mda.Universe = water.transform(aa_universe)
+
+    positions: List[np.ndarray] = [
+        _.atoms.select_atoms(selection).center_of_mass()
+        for _ in aa_universe.select_atoms("water").residues
+        for selection in water._mapping.values()
+    ]
+
     testing.assert_allclose(
-        np.array(positions),
+        np.asarray(positions),
         cg_universe.atoms.positions,
-        err_msg=native_str("The coordinates do not match."),
+        err_msg="The coordinates do not match.",
     )
 
 
 def test_tip3p_creation():
-    aa_universe = mda.Universe(TIP3P)
-    cg_universe = solvent.Tip3p(TIP3P)
-    cg_natoms = (aa_universe.select_atoms("name OW").n_atoms +
-                 aa_universe.select_atoms("name HW1").n_atoms +
-                 aa_universe.select_atoms("name HW2").n_atoms)
-    testing.assert_equal(
-        cg_universe.atoms.n_atoms,
-        cg_natoms,
-        err_msg=native_str("Number of sites don't match."),
-        verbose=True,
-    )
+    aa_universe: mda.Universe = mda.Universe(TIP3P)
+    water: solvent.Water = solvent.Tip3p()
+    water.create_topology(aa_universe)
+
+    n_atoms: int = sum(aa_universe.select_atoms(selection).residues.n_residues
+                       for selection in water._mapping.values())
+
+    testing.assert_equal(water.universe.atoms.n_atoms, n_atoms,
+                         err_msg="Number of sites don't match.", verbose=True)
 
 
 def test_tip3p_positions():
-    positions = []
-    aa_universe = mda.Universe(TIP3P)
-    cg_universe = solvent.Tip3p(TIP3P)
-    for _ in aa_universe.select_atoms("name OW HW* MW").residues:
-        positions.append(_.atoms.select_atoms("name OW MW").center_of_mass())
-        positions.append(_.atoms.select_atoms("name HW1").center_of_mass())
-        positions.append(_.atoms.select_atoms("name HW2").center_of_mass())
+    aa_universe: mda.Universe = mda.Universe(TIP4P)
+    water: solvent.Water = solvent.Water()
+    cg_universe: mda.Universe = water.transform(aa_universe)
+
+    positions: List[np.ndarray] = [
+        _.atoms.select_atoms(selection).center_of_mass()
+        for _ in aa_universe.select_atoms("water").residues
+        for selection in water._mapping.values()
+    ]
+
     testing.assert_allclose(
-        np.array(positions),
+        np.asarray(positions),
         cg_universe.atoms.positions,
-        err_msg=native_str("The coordinates do not match."),
-    )
-
-
-def test_ions_creation():
-    aa_universe = mda.Universe(IONS)
-    cg_universe = ions.SolventIons(IONS)
-    cg_natoms = (
-        aa_universe.select_atoms("name LI LIT K NA F CL BR I").n_atoms)
-    testing.assert_equal(
-        cg_universe.atoms.n_atoms,
-        cg_natoms,
-        err_msg=native_str("Number of sites don't match."),
-        verbose=True,
-    )
-
-
-def test_ions_positions():
-    positions = []
-    aa_universe = mda.Universe(IONS)
-    cg_universe = ions.SolventIons(IONS)
-    for _ in aa_universe.select_atoms("name LI LIT K NA F CL BR I").residues:
-        positions.append(
-            _.atoms.select_atoms("name LI LIT K NA F CL BR I")
-            .center_of_mass())
-    testing.assert_allclose(
-        np.array(positions),
-        cg_universe.atoms.positions,
-        err_msg=native_str("The coordinates do not match."),
+        err_msg="The coordinates do not match.",
     )
 
 
 def test_dma_creation():
-    aa_universe = mda.Universe(DMA)
-    cg_universe = solvent.Dma(DMA)
-    cg_natoms = (aa_universe.select_atoms("name C1").n_atoms +
-                 aa_universe.select_atoms("name N").n_atoms +
-                 aa_universe.select_atoms("name C2").n_atoms +
-                 aa_universe.select_atoms("name C3").n_atoms)
-    testing.assert_equal(
-        cg_universe.atoms.n_atoms,
-        cg_natoms,
-        err_msg=native_str("Number of sites don't match."),
-        verbose=True,
-    )
+    aa_universe: mda.Universe = mda.Universe(DMA)
+    dma: solvent.Water = solvent.Dma()
+    dma.create_topology(aa_universe)
+
+    n_atoms: int = sum(aa_universe.select_atoms(selection).residues.n_residues
+                       for selection in dma._mapping.values())
+
+    testing.assert_equal(dma.universe.atoms.n_atoms, n_atoms,
+                         err_msg="Number of sites don't match.", verbose=True)
 
 
 def test_dma_positions():
-    positions = []
-    aa_universe = mda.Universe(DMA)
-    cg_universe = solvent.Dma(DMA)
-    for _ in aa_universe.select_atoms("resname DMA").residues:
-        positions.append(
-            _.atoms.select_atoms("resname DMA and name C1 H1*")
-            .center_of_mass())
-        positions.append(
-            _.atoms.select_atoms("resname DMA and name C N O")
-            .center_of_mass())
-        positions.append(
-            _.atoms.select_atoms("resname DMA and name C2 H2*")
-            .center_of_mass())
-        positions.append(
-            _.atoms.select_atoms("resname DMA and name C3 H3*")
-            .center_of_mass())
+    aa_universe: mda.Universe = mda.Universe(DMA)
+    dma: solvent.Water = solvent.Dma()
+    cg_universe: mda.Universe = dma.transform(aa_universe)
+
+    positions: np.ndarray = np.asarray([
+        _.atoms.select_atoms(selection).center_of_mass()
+        for _ in aa_universe.select_atoms("resname DMA").residues
+        for selection in dma._mapping.values()
+    ])
+
     testing.assert_allclose(
-        np.array(positions),
-        cg_universe.atoms.positions,
-        err_msg=native_str("The coordinates do not match."),
+        positions, cg_universe.atoms.positions,
+        err_msg="The coordinates do not match.",
     )
